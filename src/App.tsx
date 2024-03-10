a404d47c97d37709768c78ae3e6999561981ddab
Adam Witalewski <witalewski@gmail.com>
2024-03-10 17:49:13 +0100

    Move mock data out of the App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,10 @@ import "./index.css";
 import styles from "./App.module.css";
 import MasteryCalculator from "./components/mastery-calculator/MasteryCalculator";
 import { useState } from "react";
+import { mockData } from "./mockData";
 
 function App() {
-  const [paths, setPaths] = useState([
-    {
-      id: "path-1",
-      name: "Talent Path 1",
-      talents: [
-        { id: "talent-1", name: "Talent 1", selected: true, iconIndex: 0 },
-        { id: "talent-2", name: "Talent 2", selected: true, iconIndex: 1 },
-        { id: "talent-3", name: "Talent 3", selected: false, iconIndex: 2 },
-        { id: "talent-4", name: "Talent 4", selected: false, iconIndex: 3 },
-        // { id: "talent-5", name: "Talent 5", selected: false, iconIndex: 4 },
-        // { id: "talent-6", name: "Talent 6", selected: false, iconIndex: 5 },
-        // { id: "talent-7", name: "Talent 7", selected: false, iconIndex: 6 },
-      ],
-    },
-    {
-      id: "path-2",
-      name: "Talent Path 2",
-      talents: [
-        { id: "talent-1", name: "Talent 1", selected: true, iconIndex: 4 },
-        { id: "talent-2", name: "Talent 2", selected: false, iconIndex: 5 },
-        { id: "talent-3", name: "Talent 3", selected: false, iconIndex: 6 },
-        { id: "talent-4", name: "Talent 4", selected: false, iconIndex: 7 },
-        // { id: "talent-5", name: "Talent 5", selected: false, iconIndex: 1 },
-        // { id: "talent-6", name: "Talent 6", selected: false, iconIndex: 2 },
-        // { id: "talent-7", name: "Talent 7", selected: false, iconIndex: 3 },
-      ],
-    },
-  ]);
+  const [paths, setPaths] = useState(mockData);
 
   return (
     <main className={`${styles.container} opensans`}>
---
+++ src/mockData.ts
@@ -0,0 +1,22 @@
+export const mockData = [
+  {
+    id: "path-1",
+    name: "Talent Path 1",
+    talents: [
+      { id: "talent-0", name: "Chevrons", selected: true, iconIndex: 0 },
+      { id: "talent-1", name: "Silverware", selected: true, iconIndex: 1 },
+      { id: "talent-2", name: "Cake", selected: false, iconIndex: 2 },
+      { id: "talent-3", name: "Crown", selected: false, iconIndex: 3 },
+    ],
+  },
+  {
+    id: "path-2",
+    name: "Talent Path 2",
+    talents: [
+      { id: "talent-4", name: "Boat", selected: true, iconIndex: 4 },
+      { id: "talent-5", name: "Diver", selected: false, iconIndex: 5 },
+      { id: "talent-6", name: "Energy", selected: false, iconIndex: 6 },
+      { id: "talent-7", name: "Death", selected: false, iconIndex: 7 },
+    ],
+  },
+];
